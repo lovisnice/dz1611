4e294c093aec3869bf605b0c07eda6c1eececf43
User <User@DESKTOP-S7FDFL8>
2023-11-23 22:18:37 +0200

    upd timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -8,6 +8,7 @@ const Timer = () => {
  const handleSetTime = (seconds) => {
     setCount(seconds);
     setOriginalCount(seconds);
+    setTimerStatus();
  };
 
  const handleStart = () => {
@@ -26,23 +27,71 @@ const Timer = () => {
 
  useEffect(() => {
     let interval;
-    if (timerStatus === 'started') {
-      interval = setInterval(() => {
-        setCount((prevCount) => prevCount - 1);
-      }, 1000);
+    if(originalCount !== 0 && originalCount >= 0)
+    {
+        if (timerStatus === 'started') {
+            interval = setInterval(() => {
+                    setCount((prevCount) => {
+                        if (prevCount !== 0) {
+                          return prevCount - 1;
+                        } else {
+                          clearInterval(interval); // Stop the interval when prevCount reaches 0
+                          setTimerStatus('paused');
+                          return 0;
+                        }
+                      });
+                    }, 1000);
+          }
     }
+    
     return () => clearInterval(interval);
  }, [timerStatus]);
 
  return (
-    <div>
-      <input type="number" onChange={(e) => handleSetTime(e.target.value)} />
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handlePause}>Pause</button>
-      <button onClick={handleReset}>Reset</button>
-      <p>{count}</p>
-    </div>
+    <div style={styles.container}>
+        <p style={styles.timer}>{count}</p>
+    <input
+      type="number"
+      onChange={(e) => handleSetTime(e.target.value)}
+      style={styles.input}
+    />
+     <div style={styles.buttonContainer}>
+        <button onClick={handleStart} style={styles.button}>Start</button>
+        <button onClick={handlePause} style={styles.button}>Pause</button>
+        <button onClick={handleReset} style={styles.button}>Reset</button>
+      </div>
+    
+  </div>
  );
 };
+
+const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      marginTop: '20px',
+    },
+    buttonContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '10px',
+      },
+    input: {
+      margin: '10px',
+      padding: '5px',
+      fontSize: '16px',
+    },
+    button: {
+      margin: '5px',
+      padding: '10px',
+      fontSize: '16px',
+      cursor: 'pointer',
+    },
+    timer: {
+      fontSize: '24px',
+      fontWeight: 'bold',
+    },
+  };
 
 export default Timer;
